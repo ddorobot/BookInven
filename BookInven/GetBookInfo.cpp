d83f431256c48eb6fab292238f86b9f097022dbd
Juhong <>
2018-12-03 02:13:53 +0900

    MSVS 프로젝트에 boost library path 추가 GetBookInfo class에 boost 사용을 위한 헤더파일 추가

---
+++ BookInven/GetBookInfo.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "GetBookInfo.h"
+
+
+GetBookInfo::GetBookInfo()
+{
+}
+
+
+GetBookInfo::~GetBookInfo()
+{
+}
+
+BookInfo GetBookInfo::GetInfo(const std::string str)
+{
+	BookInfo info;
+
+	return info;
+}
--- BookInven/GetBookInfo.h
+++ BookInven/GetBookInfo.h
@@ -4,12 +4,22 @@
 
 #include <string>
 
+#include <boost/thread.hpp>
+
+#include "boost\thread\detail\thread.hpp"
+#include "boost\property_tree\ptree.hpp"
+#include "boost\property_tree\ini_parser.hpp"
+#include "boost\filesystem\operations.hpp"
+#include "boost\range\iterator_range.hpp"
+
+#include "boost\system\error_code.hpp"
+
 class GetBookInfo
 {
 public:
 	GetBookInfo();
 	~GetBookInfo();
 
-	BookInfo GetInfo(const std::string str, )
+	BookInfo GetInfo(const std::string str);
 };
 
